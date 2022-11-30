bb387d96e37afbf5f6ced1d1d177027fa65fa8d2
AlekBenni <adeyva@mail.ru>
2022-11-30 13:26:56 +0400

    commit

---
+++ src/01/test.tsx
@@ -0,0 +1,3 @@
+console.log("TEST PAGE");
+
+export const Test = "Test";
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Value from './01/01';
+
+function App() {
+  return (
+    <div className="App">
+        <Value/>
+    </div>
+  );
+}
+
+export default App;
